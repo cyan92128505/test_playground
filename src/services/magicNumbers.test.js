6d711d86b651c999e8681e468e30b00301d7b0a6
cyan92128505 <cyan92128505@gmail.com>
2022-07-02 15:19:57 +0800

    Rename service:magicNumbers

--- src/services/magicNumbers.test.js
+++ src/services/magicNumbers.test.js
@@ -1,16 +1,16 @@
-import magicNumbers from "./magic";
+import MagicNumbers from "./magicNumbers";
 
 test("should can be divided by 3", () => {
-  let result = magicNumbers(3);
+  let result = MagicNumbers(3);
   expect(result).toContain(3);
 });
 
 test("should can be divided by 7", () => {
-  let result = magicNumbers(7);
+  let result = MagicNumbers(7);
   expect(result).toContain(7);
 });
 
 test("should cannot be divided by both 3 and 7", () => {
-  let result = magicNumbers(21);
+  let result = MagicNumbers(21);
   expect(result).not.toContain(21);
 });
