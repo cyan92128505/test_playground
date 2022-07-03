ada316e72bde92c1172e356abe5befa68c940a9d
cyan92128505 <cyan92128505@gmail.com>
2022-07-03 22:06:39 +0800

    Modify test of utils

--- src/utils/magicNumbers.test.js
+++ src/utils/magicNumbers.test.js
@@ -1,16 +1,18 @@
 import MagicNumbers from "./magicNumbers";
 
-test("should can be divided by 3", () => {
-  let result = MagicNumbers(3);
-  expect(result).toContain(3);
-});
+describe("Test of MagicNumbers", () => {
+  test("should can be divided by 3", () => {
+    let result = MagicNumbers(3);
+    expect(result).toContain(3);
+  });
 
-test("should can be divided by 7", () => {
-  let result = MagicNumbers(7);
-  expect(result).toContain(7);
-});
+  test("should can be divided by 7", () => {
+    let result = MagicNumbers(7);
+    expect(result).toContain(7);
+  });
 
-test("should cannot be divided by both 3 and 7", () => {
-  let result = MagicNumbers(21);
-  expect(result).not.toContain(21);
+  test("should cannot be divided by both 3 and 7", () => {
+    let result = MagicNumbers(21);
+    expect(result).not.toContain(21);
+  });
 });
--- src/utils/validParentheses.test.js
+++ src/utils/validParentheses.test.js
@@ -1,16 +1,18 @@
 import ValidParentheses from "./validParentheses";
 
-test("should '{}([])' can be Valid", () => {
-  let result = ValidParentheses("{}([])");
-  expect(result).toBe(true);
-});
+describe("Test of ValidParentheses", () => {
+  test("should '{}([])' can be Valid", () => {
+    let result = ValidParentheses("{}([])");
+    expect(result).toBe(true);
+  });
 
-test("should '{[]({})}' can be Valid", () => {
-  let result = ValidParentheses("{[]({})}");
-  expect(result).toBe(true);
-});
+  test("should '{[]({})}' can be Valid", () => {
+    let result = ValidParentheses("{[]({})}");
+    expect(result).toBe(true);
+  });
 
-test("should '(]' can be Invalid", () => {
-  let result = ValidParentheses("(]");
-  expect(result).toBe(false);
+  test("should '(]' can be Invalid", () => {
+    let result = ValidParentheses("(]");
+    expect(result).toBe(false);
+  });
 });
